9069cdb432ee6b133202ab859c40387721ef3cc5
Dorahontas <learner-11acd1@extensionschool.ch>
2020-06-03 21:10:38 +0200

    Functionality for attaching creation dates to the notes and comments added, style adjustments.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask
+import datetime
 
 # create an instance of the Flask application
 app = flask.Flask(__name__)
@@ -41,11 +42,16 @@ def index():
 def add_note():
   note_text = flask.request.args.get('note') # process the html get request
   if note_text: # if there is a request for adding a new note
+    note_text = note_text + '##' + str(datetime.datetime.now())[0:16]
     add_text_to_file('notes_database.txt', note_text) # append a new line to the notes database file
 
   index_html = load_file('index.html') # load the file index.html
 
   add_note_html = load_file('templates/add_note.html') # load the file templates/add_note.html
+  if note_text:
+    add_note_html = add_note_html.replace('## NOTE_ADDED_STATUS ##', 'Note added successfully')
+  else:
+    add_note_html = add_note_html.replace('## NOTE_ADDED_STATUS ##', '')
 
   # replace '## INDEX_HTML ##' with the content of templates/add_note_html
   index_html = index_html.replace('## INDEX_HTML ##', add_note_html)
@@ -59,12 +65,15 @@ def all_notes():
   note_html = load_file('templates/note.html') # load the file templates/note.html
   notes = load_file('notes_database.txt') # load all the notes from the notes_database.txt file
   notes = notes.split('\n') # split the notes into a list of individual notes (the separator is '\n')
+  notes.reverse()
 
   notes_html = '' # create an empty string where all the notes formatted in html will be added
   for note in notes: # iterate through all the notes loaded from the datebase file
     if note != '': # process the note only if it's not empty
-      notes_html = notes_html + note_html.replace('## NOTE_TEXT ##', note) # format the text of the note in html and add it to the result notes_html
-      notes_html = notes_html + '<br>' # add a horizontal break after every note
+      note = note.split('##')
+      aux = note_html.replace('## NOTE_TEXT ##', note[0]) # format the text of the note in html and add it to the result notes_html
+      aux = aux.replace('## DATE ##', note[1])
+      notes_html = notes_html + aux + '<br>' # format the text of the note in html and add it to the result notes_html
 
   all_notes_html = load_file('templates/all_notes.html') # load the file templates/all_notes.html
   all_notes_html = all_notes_html.replace('## ALL_NOTES_HTML ##', notes_html) # insert the notes in html format to the template all_notes page
@@ -89,9 +98,11 @@ def search_notes():
   notes_found_html = '' # create an empty string where all the found notes formatted in html will be stored
   if search_text: # proceed only if the search_text is not empty
     for note in notes: # iterate through all the notes
-      if search_text.lower() in note.lower(): # if the searched text is in the note (case insensitive search)
-        # append the note_html with the NOTE_TEXT properly replaced to the notes_found_html
-        notes_found_html = notes_found_html + note_html.replace('## NOTE_TEXT ##', note) + '<br>'
+      note = note.split('##')
+      if search_text.lower() in note[0].lower(): # if the searched text is in the note (case insensitive search)
+        aux = note_html.replace('## NOTE_TEXT ##', note[0])
+        aux = aux.replace('## DATE ##', note[1])
+        notes_found_html = notes_found_html + aux + '<br>'
 
   search_notes_html = load_file('templates/search_notes.html') # load the file templates/search_notes.html
   search_notes_html = search_notes_html.replace('## NOTES_FOUND_HTML ##', notes_found_html)
@@ -111,7 +122,7 @@ def comments():
   comment_type = flask.request.args.get('comment_type') # process the html get request
 
   if comment_text and comment_type:
-    comment_with_type = comment_type + '##' + comment_text
+    comment_with_type = comment_type + '##' + comment_text + '##' + str(datetime.datetime.now())[:-10]
     add_text_to_file('comments_database.txt', comment_with_type)
 
   comment_positive_html = load_file('templates/comment_positive.html') # load the file templates/comment_positive.html
@@ -119,17 +130,20 @@ def comments():
 
   comments = load_file('comments_database.txt') # load all the comments from the comments_database.txt file
   comments = comments.split('\n') # split the comments into a list of individual comments (the separator is '\n')
+  comments.reverse()
 
   comments_html = '' # create an empty string where all the comments formatted in html will be added
   for comment in comments: # iterate through all the notes loaded from the datebase file
     if comment != '':
       comment = comment.split('##') # split the type (positive or negative) and the text
-      if comment[0] == 'positive': # in case of positive comment add a positive comment
-        # append the properly formatted positive comment to the comments_html valriable
-        comments_html = comments_html + comment_positive_html.replace('## COMMENT_TEXT ##', comment[1]) + '<br>'
-      if comment[0] == 'negative': # in case of negative comment add a negative comment
-        # append the properly formatted negative comment to the comments_html valriable
-        comments_html = comments_html + comment_negative_html.replace('## COMMENT_TEXT ##', comment[1]) + '<br>'
+      if comment[0] == 'positive':
+        aux = comment_positive_html.replace('## COMMENT_TEXT ##', comment[1])
+        aux = aux.replace('## DATE ##', comment[2])
+        comments_html = comments_html + aux + '<br>'
+      if comment[0] == 'negative':
+        aux = comment_negative_html.replace('## COMMENT_TEXT ##', comment[1])
+        aux = aux.replace('## DATE ##', comment[2])
+        comments_html = comments_html + aux + '<br>'
 
   all_comments_html = load_file('templates/comments.html') # load the file templates/all_notes.html
   all_comments_html = all_comments_html.replace('## COMMENTS_HTML ##', comments_html) # insert the comments in html format to the template comments page
